fb2cbfb24db1879b70fda586273d4040766718f4
Julia <juliakhrabatyn12@gmail.com>
2026-03-12 02:49:23 +0200

    Add accounts app, create validators for  accounts .models , create User model and Follow model

---
+++ accounts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from core.models import AbstractBaseModel
+from accounts.validators import validate_birth_day
+from django.core.exceptions import ValidationError
+
+
+# Create your models here.
+class User(AbstractUser, AbstractBaseModel):
+    """Represent user"""
+
+    birth_date = models.DateField(validators=[validate_birth_day])
+    country = models.CharField(max_length=2)  # Country Code
+    bio = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+
+class Follow(AbstractBaseModel):
+    """Represent user's followers and user's followings."""
+
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+
+    def clean(self):
+        if self.follower == self.following:
+            raise ValidationError("You can not follow yourself!")
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
+
+    class Meta:
+        unique_together = [("follower", "following")]
+        verbose_name = "Follow"
+        verbose_name_plural = "Follows"
---
+++ accounts/validators.py
@@ -0,0 +1,20 @@
+from datetime import date
+from django.core.exceptions import ValidationError
+
+
+def validate_birth_day(value: date):
+    """
+    Validate if birth_day value from the past
+    and raise ValidationError when it is from the future
+    or user is younger that 13 y.o.
+    """
+    today = date.today()
+    age = (
+        today.year
+        - value.year
+        - ((today.month, today.day) < (value.month, value.day))
+    )
+    if value > today:
+        raise ValidationError("Birth date can not be from the future!")
+    if age < 13:
+        raise ValidationError("Birth date can not be less than 13 y.o. !")
