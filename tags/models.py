9c51987fccf3531a2058ee68cc0bd4d1754713dc
Julia <juliakhrabatyn12@gmail.com>
2026-03-13 16:40:41 +0200

    Add Tag model

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models import AbstractBaseModel
+
 
 # Create your models here.
+class Tag(AbstractBaseModel):
+    """Represent Tag object"""
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title.lower()
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        indexes = [models.Index(fields=["title"])]
