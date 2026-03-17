b7cdab6bf55fecf9d3083de21448d597bca70746
Julia <juliakhrabatyn12@gmail.com>
2026-03-17 18:50:34 +0200

    install django-extensions

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django_countries",
     "constance",
     "constance.backends.database",
+    "django_extensions",
 ]
 
 CONSTANCE_BACKEND = "constance.backends.database.DatabaseBackend"
