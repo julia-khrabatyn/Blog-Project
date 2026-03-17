216fa40046c469f4be7d2104ef361ba27f39fd1f
Julia <juliakhrabatyn12@gmail.com>
2026-03-17 03:24:35 +0200

    corrected blog models

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
-from core.models import AbstractBaseModel, PublishMixin, SlugMixin
 
 from accounts.models import User
+from core.models import AbstractBaseModel, PublishMixin, SlugMixin
 
 from blog.validators import validate_image_file
 
@@ -58,9 +58,7 @@ class Post(AbstractBaseModel, PublishMixin, SlugMixin):
         verbose_name_plural = "Posts"
         indexes = [
             models.Index(fields=["title"]),
-            models.Index(fields=["published"]),
             models.Index(fields=["user", "published"]),
-            models.Index(fields=["published", "created_at"]),
             models.Index(fields=["user", "published", "created_at"]),
         ]
 
@@ -76,11 +74,20 @@ class Image(AbstractBaseModel):
             ),
             validate_image_file,
         ],
+        help_text="Upload your image (use can use only .png, .jpeg, .jpg, .webp files)",
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="images"
+        Post,
+        on_delete=models.CASCADE,
+        related_name="images",
+        help_text="This image belongs to (post)",
     )
-    alt_text = models.CharField(max_length=100, null=True, blank=True)
+    alt_text = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        help_text="Alternative image text",
+    )
 
     def __str__(self):
         return self.alt_text.title() if self.alt_text else str(self.image_file)
@@ -88,21 +95,26 @@ class Image(AbstractBaseModel):
     class Meta:
         verbose_name = "Image"
         verbose_name_plural = "Images"
-        indexes = [models.Index(fields=["post"])]
 
 
 class Like(AbstractBaseModel):
     """Represents user preferences for a specific post."""
 
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="likes"
+        User,
+        on_delete=models.CASCADE,
+        related_name="likes",
+        help_text="Like from user",
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="likes"
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes",
+        help_text="like to post",
     )
 
     def __str__(self):
-        return f"{self.user} likes {self.post.title.title()}"
+        return f"{self.user} likes {self.post}"
 
     class Meta:
         constraints = [
@@ -114,11 +126,10 @@ class Like(AbstractBaseModel):
         verbose_name_plural = "Likes"
 
 
-class Category(AbstractBaseModel):
+class Category(AbstractBaseModel, SlugMixin):
     """Represent a post category/categories."""
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
+    title = models.CharField(max_length=255, help_text="Your category title")
 
     @property
     def posts_count(self):
@@ -134,5 +145,4 @@ class Category(AbstractBaseModel):
         verbose_name_plural = "Categories"
         indexes = [
             models.Index(fields=["title"]),
-            models.Index(fields=["slug"]),
         ]
