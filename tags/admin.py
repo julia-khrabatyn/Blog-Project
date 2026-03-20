f555ffd60b833d5e9395f818b22e90cf6a82e206
Julia <juliakhrabatyn12@gmail.com>
2026-03-20 17:57:20 +0200

    Register Tag model in django-admin

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from tags.models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Register Tag model in django-admin."""
+
+    list_display = ("title", "slug", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = (
+        "title",
+        "updated_at",
+    )
+    fieldsets = (
+        (
+            "Tags info",
+            {
+                "fields": ("title", "slug", "created_at", "updated_at"),
+            },
+        ),
+    )
