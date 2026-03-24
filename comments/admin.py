8607aab6cc28bfeefd4d1dabdecb425383636eed
Julia <juliakhrabatyn12@gmail.com>
2026-03-24 03:51:53 +0200

    add coment admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Register Comment model in django-admin."""
+
+    list_display = ("user", "post", "text")
+    ordering = ["-updated_at"]
+    readonly_fields = ("created_at", "updated_at")
+    list_filter = ("user", "post", "updated_at")
+    fieldsets = (
+        (
+            "Who created",
+            {
+                "fields": ("user",),
+            },
+        ),
+        ("To which post", {"fields": ("post",)}),
+        (
+            "Date of creation/updation",
+            {"fields": ("created_at", "updated_at")},
+        ),
+        ("Comment text", {"fields": ("text",)}),
+    )
