2d98d5eca0dd4ac1d367d0eb71e375e304720aab
Julia <juliakhrabatyn12@gmail.com>
2026-04-08 01:51:08 +0300

    add home.html template, create HomeView, extend base.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import HomeView, PostListView
 
 urlpatterns = [
     path("posts/list/", PostListView.as_view(), name="post_list"),
+    path("home/", HomeView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from django.views.generic import ListView
-from .models import Post
+
+from .models import Post, Category
 
 
 class PostListView(ListView):
@@ -19,3 +21,23 @@ class PostListView(ListView):
             .select_related("user")
             .order_by("-updated_at")
         )
+
+
+class HomeView(ListView):
+    """Display main page of Byline-Blog."""
+
+    model = Post
+    template_name = "home.html"
+    context_object_name = "latest_posts"
+
+    def get_queryset(self):
+        """Show users only 3 latest published posts."""
+        return Post.objects.filter(published=True).order_by("-updated_at")[:3]
+
+    def get_context_data(self, **kwargs):
+        """Show sorted categories by popularity (number of posts in category)"""
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.annotate(
+            posts_count=Count("posts")
+        ).order_by("-posts_count")[:5]
+        return context
