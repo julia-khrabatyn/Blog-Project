ca31ce572cf6edd950691552101904a71805af22
Julia <juliakhrabatyn12@gmail.com>
2026-04-19 00:36:49 +0300

    edit urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from .views import AuthorPostsListView, HomeView, PostDetailView, PostListView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="post_list"),
+    path("posts/", PostListView.as_view(), name="post_list"),
     path(
         "author/<str:username>",
         AuthorPostsListView.as_view(),
         name="author_posts",
     ),
-    path("home/", HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
     path(
         "post/<uuid:pk>/",
         PostDetailView.as_view(),
