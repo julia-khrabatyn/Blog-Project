c7bc0a9961c1450ad236a39f6994692a54a56f2a
Julia <juliakhrabatyn12@gmail.com>
2026-05-01 01:25:31 +0300

    create templatetags in accounts and move logic about extracting users avatar with all checks into user_tags.py -> thus rewrite _user_avatar.html

---
+++ accounts/templatetags/user_tags.py
@@ -0,0 +1,35 @@
+from django import template
+from django.conf import settings
+from django.templatetags.static import static
+
+from constance import config
+
+register = template.Library()
+
+__all__ = [
+    "get_user_avatar",
+]
+
+
+@register.simple_tag
+def get_user_avatar(user_obj):
+    """Finction for getting user's avatar and return it for displaying it in templates."""
+
+    # extract user's avatar if user upload it by himself.
+    if hasattr(user_obj, "avatar") and user_obj.avatar:
+        return user_obj.avatar.url
+
+    # if user didn't upload avatar, but logged in using Google social account and have profile avatar in it -> get it.
+
+    social = user_obj.socialaccount_set.filter(provider="google").first()
+    if social:
+        google_url = social.extra_data.get("picture")
+        if google_url:
+            return google_url
+
+    # if neither user's avatar, nor google avatar -> then give to user default byline avatar.
+    default_path = config.DEFAULT_AVATAR
+    if default_path.startswith("images/"):
+        return static(default_path)
+
+    return f"{settings.MEDIA_URL}{default_path}"
